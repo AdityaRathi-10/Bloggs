1913085965858036ed5f6a04a20175702bf4a8e2
AdityaRathi-10 <adityarathi3916@gmail.com>
2026-03-21 23:34:17 +0530

    added route for checking unique username

---
+++ src/app/api/sign-up/check-unique/route.ts
@@ -0,0 +1,42 @@
+import User from "@/models/User";
+import dbConnect from "@/utils/dbConnet";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const username = request.nextUrl.searchParams.get("username");
+    
+    if (!username) {
+        return NextResponse.json(
+            { success: false, message: "Username is not provided" },
+            { status: 400 }
+        )
+    }
+    
+    await dbConnect();
+
+    try {
+        if(username) {
+            const user = await User.findOne({ 
+                username: { $regex: `^${username}$`, $options: "i" }
+            }).select("username").lean();
+
+            if(user) {
+                return NextResponse.json(
+                    { success: false, message: "Username is already taken" },
+                    { status: 400 }
+                )
+            }
+
+            return NextResponse.json(
+                { success: true, message: "Username is available" },
+                { status: 200 }
+            )
+        }
+    } catch (error) {
+        void error
+        return NextResponse.json(
+            { success: false, message: "Error checking username" },
+            { status: 500 }
+        )
+    }
+}
